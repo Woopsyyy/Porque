c233c1d43123695b4f8ae5b14007591b733a245a
Woopsyyy <yoshiru28@gmail.com>
2026-06-14 15:07:31 +0800

    fix: convert PNG mascot to ICO format dynamically in memory for Windows system tray

--- internal/systray/systray_windows.go
+++ internal/systray/systray_windows.go
@@ -3,6 +3,8 @@
 package systray
 
 import (
+	"bytes"
+
 	"github.com/getlantern/systray"
 )
 
@@ -14,7 +16,13 @@ var (
 
 // Start initializes the system tray menu.
 func Start(icon []byte, showCb func(), quitCb func()) {
-	iconBytes = icon
+	// If the provided icon is a PNG, wrap it in a standard ICO header for Windows compatibility
+	if isPNG(icon) {
+		iconBytes = pngToIco(icon)
+	} else {
+		iconBytes = icon
+	}
+	
 	onShow = showCb
 	onQuit = quitCb
 
@@ -48,4 +56,49 @@ func onReady() {
 			}
 		}
 	}()
+}
+
+func isPNG(data []byte) bool {
+	return len(data) > 8 && bytes.Equal(data[:8], []byte{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A})
+}
+
+// pngToIco wraps a raw PNG image inside a single-entry Windows ICO container in memory
+func pngToIco(pngBytes []byte) []byte {
+	size := len(pngBytes)
+	ico := make([]byte, 22+size)
+
+	// Icon Header
+	ico[0] = 0 // Reserved
+	ico[1] = 0
+	ico[2] = 1 // Type (1 = Icon)
+	ico[3] = 0
+	ico[4] = 1 // Count (1 image)
+	ico[5] = 0
+
+	// Directory Entry
+	ico[6] = 0   // Width (0 means 256px)
+	ico[7] = 0   // Height (0 means 256px)
+	ico[8] = 0   // Color count (0 = no palette)
+	ico[9] = 0   // Reserved
+	ico[10] = 1  // Planes
+	ico[11] = 0
+	ico[12] = 32 // Bit count (32 bits)
+	ico[13] = 0
+
+	// Bytes size (4 bytes, little-endian)
+	ico[14] = byte(size)
+	ico[15] = byte(size >> 8)
+	ico[16] = byte(size >> 16)
+	ico[17] = byte(size >> 24)
+
+	// Image offset (4 bytes, little-endian) - always 22
+	ico[18] = 22
+	ico[19] = 0
+	ico[20] = 0
+	ico[21] = 0
+
+	// Image Data
+	copy(ico[22:], pngBytes)
+
+	return ico
 }
